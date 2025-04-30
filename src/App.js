c5e831c675b2a165adfea48f4b4d80ac98bc0e3f
Your name <your.email@yourmailserver.com>
2025-04-30 11:50:20 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
+
 import Navbar from './components/Navbar';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
@@ -15,11 +17,13 @@ function App() {
 
         {/* Define routes for each section */}
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/payment" element={<Payment />} />
-        </Routes>
+  <Route path="/" element={<Home />} />
+  <Route path="/about" element={<AboutUs />} />
+  <Route path="/contact" element={<Contact />} />
+  <Route path="/payment" element={<Payment />} />
+  <Route path="*" element={<Navigate to="/" />} />  {/* Add this */}
+</Routes>
+
 
         {/* Footer is usually global, so render it on all pages */}
         <Footer />
